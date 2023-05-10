78e23a88098c996283d14c54ec0d082f31431ece
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2023-05-10 17:20:54 +0200

    Archivos que faltaban del commit anterior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, map, shareReplay } from 'rxjs';
+import { ListasCompraService } from './services/listas-compra.service';
+import { ListaCompra } from './models/lista-compra';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  listasCompra: ListaCompra[] = [];
+
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
@@ -15,5 +20,37 @@ export class AppComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private listasCompraService: ListasCompraService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const lista: ListaCompra = {
+      nombre: 'Carrefour',
+      productos: []
+    }
+
+    this.listasCompraService.createListaCompra(lista);
+
+    /* this.listasCompraService.getListasCompra()
+      .then((listas: ListaCompra[]) => {
+        this.listasCompra = listas;
+        console.log(listas);
+      }
+    ).catch((error: string) => console.log(error)); */
+
+    try {
+      this.listasCompra = await this.listasCompraService.getListasCompra();
+    } catch(exception) {
+      console.log(exception);
+    }
+
+    console.log('Después de la petición al servicio');
+
+    this.listasCompraService.getDatos().subscribe({
+      next: (valor: number) => console.log(valor),
+      error: (error) => console.log(error),
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './components/navigation/navigation.component';
+
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,6 +26,7 @@ import { MatListModule } from '@angular/material/list';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
