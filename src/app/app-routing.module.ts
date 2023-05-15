db2c3339a79527f7e769244d436ad78bcc9ca55a
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2023-05-15 17:13:11 +0200

    Añadidos Guards para proteger rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductosComponent } from './components/productos/productos.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { AuthGuardGuard } from './guards/auth-guard.guard';
 
 const routes: Routes = [
-  { path: '', component: ProductosComponent },
+  { path: '', component: ProductosComponent, canActivate: [AuthGuardGuard] },
   { path: 'listas-compra', redirectTo: '/', pathMatch: 'full' },
-  { path: 'listas-compra/:id', component: ProductosComponent },
+  { path: 'listas-compra/:id', component: ProductosComponent, canActivate: [AuthGuardGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent }
   // { path: '**', component: PageNotFoundComponent }
