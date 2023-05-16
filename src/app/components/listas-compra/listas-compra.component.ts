c0eec1abc26ced30d6181a03de846161ebb28b10
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2023-05-16 17:16:21 +0200

    Integrados los selectores al componente ListasCompra para obtener datos.

--- src/app/components/listas-compra/listas-compra.component.ts
+++ src/app/components/listas-compra/listas-compra.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { NuevaListaComponent } from 'src/app/dialogs/nueva-lista/nueva-lista.component';
 import { ListaCompra } from 'src/app/models/lista-compra';
 import { ListasCompraService } from 'src/app/services/listas-compra.service';
-import { cargarListas } from 'src/app/states/actions/listas-compra.actions';
+import { cargarListas, listasCargadas } from 'src/app/states/actions/listas-compra.actions';
+import { loadingListasCompraSelector } from 'src/app/states/selectors/listas-compra.selectors';
 
 @Component({
   selector: 'app-listas-compra',
@@ -13,6 +15,7 @@ import { cargarListas } from 'src/app/states/actions/listas-compra.actions';
 })
 export class ListasCompraComponent implements OnInit {
   listasCompra: ListaCompra[] = [];
+  loading$: Observable<boolean> = new Observable();
 
   constructor(
     private listasCompraService: ListasCompraService,
@@ -21,7 +24,18 @@ export class ListasCompraComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.loading$ = this.store.select(loadingListasCompraSelector);
+
     this.store.dispatch(cargarListas());
+
+    this.listasCompraService.getListasCompra().subscribe({
+      next: (respuesta: any) => {
+        if (respuesta.success) {
+          this.store.dispatch(listasCargadas({ items: respuesta.data }));
+        }
+      },
+      error: (error) => console.log(error)
+    });
 
     /* this.listasCompraService.getListasCompra().subscribe({
       next: (respuesta: any) => {
--- src/app/states/reducers/listas-compra.reducers.ts
+++ src/app/states/reducers/listas-compra.reducers.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from "@ngrx/store"
-import { cargarListas } from "../actions/listas-compra.actions"
+import { cargarListas, listasCargadas } from "../actions/listas-compra.actions"
 import { ListasCompraState } from "../models/listas-compra.state";
 
 export const initialState: ListasCompraState = {
@@ -10,6 +10,9 @@ export const initialState: ListasCompraState = {
 export const listasCompraReducer = createReducer(
   initialState,
   on(cargarListas, (state) => {
-    return {...state, loading: true}
+    return {...state, loading: true};
+  }),
+  on(listasCargadas, (state, {items}) => {
+    return {...state, loading: false, items};
   })
 );
